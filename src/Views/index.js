c2ce6ee8abc05ba8a09132b28cfa57ea5a077ede
Fatima Tabassum <fatimatabassum743@gmail.comm>
2025-03-01 00:00:34 +0530

    resolve issue

--- src/Views/index.js
+++ src/Views/index.js
@@ -48,67 +48,67 @@ const Signup = lazy(() => import("./PreLogin/Signup"));
 const ForgotPassword = lazy(() => import("./PreLogin/ForgotPassword"));
 const OtpVerification = lazy(() => import("./PreLogin/otpVerification"));
 const NewPassword = lazy(() => import("./PreLogin/newPassword"));
-
+ // Protected Route Wrapper: Ensures user is authenticated
+ const ProtectedRoute = ({ element,isAuthenticated }) => {
+  return isAuthenticated ? element : <Navigate to={`${PREFIX_AUTH_PATH}/signin`} />;
+};
 const Views = () => {
   // Accessing authentication state
   const { isAuthenticated } = useContext(AuthContext);
 
-  // Protected Route Wrapper: Ensures user is authenticated
-  const ProtectedRoute = ({ element }) => {
-    return isAuthenticated ? element : <Navigate to={`${PREFIX_AUTH_PATH}/signin`} />;
-  };
+ 
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {/* Protected Routes */}
-        <Route exact path={`${PREFIX_APP_PATH}/dashboard`} element={<ProtectedRoute element={<Dashboards />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/inventory`} element={<ProtectedRoute element={<Inventory />} />} />
-        {/* <Route exact path={`${PREFIX_APP_PATH}/inventory/addItem`} element={<ProtectedRoute element={<AddItem />} />} /> */}
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/addItem`} element={<ProtectedRoute element={<AddItemForm/>} />} />
+        <Route path="/app/dashboard" element={isAuthenticated ? <Dashboards /> : <Navigate to="/auth/signin" />} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory`} element={<ProtectedRoute element={<Inventory />} isAuthenticated={isAuthenticated} />} />
+        {/* <Route  path={`${PREFIX_APP_PATH}/inventory/addItem`} element={<ProtectedRoute element={<AddItem />} />} /> */}
+        <Route  path={`${PREFIX_APP_PATH}/inventory/addItem`}element={<ProtectedRoute element={<AddItemForm/>} isAuthenticated={isAuthenticated}/>} />
 
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/adjustments`} element={<ProtectedRoute element={<InventoryAdjustments />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/create-adjustment`} element={<ProtectedRoute element={<CreateAdjustment />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/new-adjustment`} element={<ProtectedRoute element={<NewAdjustment />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/edit`} element={<ProtectedRoute element={<Edit />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/pincodes`} element={<ProtectedRoute element={<Pincodes />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/pincodes/addpincode`} element={<ProtectedRoute element={<AddEditpincode />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/pincodes/Editpincode`} element={<ProtectedRoute element={<AddEditpincode />} />} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory/adjustments`} element={<ProtectedRoute element={<InventoryAdjustments />} isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory/create-adjustment`} element={<ProtectedRoute element={<CreateAdjustment />} isAuthenticated={isAuthenticated}/>} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory/new-adjustment`} element={<ProtectedRoute element={<NewAdjustment />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory/edit`} element={<ProtectedRoute element={<Edit />} />}isAuthenticated={isAuthenticated} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory/pincodes`} element={<ProtectedRoute element={<Pincodes />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory/pincodes/addpincode`} element={<ProtectedRoute element={<AddEditpincode />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory/pincodes/Editpincode`} element={<ProtectedRoute element={<AddEditpincode />} isAuthenticated={isAuthenticated}/>} />
 
-        <Route exact path={`${PREFIX_APP_PATH}/inventory/pincodes/viewpincode`} element={<ProtectedRoute element={<Viewpincode />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/customers`} element={<ProtectedRoute element={<Customers />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/add-new-customer`} element={<ProtectedRoute element={<AddNewCustomer />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/products`} element={<ProtectedRoute element={<Products />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/orders`} element={<ProtectedRoute element={<Orders />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/runsheet`} element={<ProtectedRoute element={<Runsheet />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/runsheet/viewRunSheet/:id`} element={<ProtectedRoute element={<ViewRunsheet />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/runsheet/createRunSheet`} element={<ProtectedRoute element={<CreateRunsheet />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/riderSummary`} element={<ProtectedRoute element={<RiderSummary />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/riderSummary/onboarding`} element={<ProtectedRoute element={<Onboarding />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/riderSummary/onboarding/riderDetails/:id`} element={<ProtectedRoute element={<RiderDetails />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/riderSummary/onboarding/approveRider/:id`} element={<ProtectedRoute element={<ApproveRider />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/collectionPayment`} element={<ProtectedRoute element={<CollectionPayment />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/logistics/collectionPayment/view-details/:id`} element={<ProtectedRoute element={<ViewDetailsPage />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/purchaseOrders`} element={<ProtectedRoute element={<PurchaseOrders />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings`} element={<ProtectedRoute element={<Settings />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac`} element={<ProtectedRoute element={<Alloptions />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/creategroup`} element={<ProtectedRoute element={<Creategroup />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/createpolicy`} element={<ProtectedRoute element={<Createpolicy />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/viewpolicy`} element={<ProtectedRoute element={<Viewpolicy />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/users`} element={<ProtectedRoute element={<UsersContent />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/roles`} element={<ProtectedRoute element={<RolesContent />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/createuser`} element={<ProtectedRoute element={<Createuser />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/groups`} element={<ProtectedRoute element={<GroupsContent />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/groups/viewgroupdetail`} element={<ProtectedRoute element={<Viewgroupdetail />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/policies`} element={<ProtectedRoute element={<PoliciesContent />} />} />
-        <Route exact path={`${PREFIX_APP_PATH}/settings/rbac/permissions`} element={<ProtectedRoute element={<PermissionsContent />} />} />
+        <Route  path={`${PREFIX_APP_PATH}/inventory/pincodes/viewpincode`} element={<ProtectedRoute element={<Viewpincode />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/customers`} element={<ProtectedRoute element={<Customers />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/add-new-customer`} element={<ProtectedRoute element={<AddNewCustomer />} isAuthenticated={isAuthenticated}/>} />
+        <Route  path={`${PREFIX_APP_PATH}/products`} element={<ProtectedRoute element={<Products />} isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/orders`} element={<ProtectedRoute element={<Orders />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/runsheet`} element={<ProtectedRoute element={<Runsheet />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/runsheet/viewRunSheet/:id`} element={<ProtectedRoute element={<ViewRunsheet />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/runsheet/createRunSheet`} element={<ProtectedRoute element={<CreateRunsheet />} isAuthenticated={isAuthenticated}/>} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/riderSummary`} element={<ProtectedRoute element={<RiderSummary />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/riderSummary/onboarding`} element={<ProtectedRoute element={<Onboarding />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/riderSummary/onboarding/riderDetails/:id`} element={<ProtectedRoute element={<RiderDetails />} isAuthenticated={isAuthenticated}/>} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/riderSummary/onboarding/approveRider/:id`} element={<ProtectedRoute element={<ApproveRider />} isAuthenticated={isAuthenticated}/>} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/collectionPayment`} element={<ProtectedRoute element={<CollectionPayment />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/logistics/collectionPayment/view-details/:id`} element={<ProtectedRoute element={<ViewDetailsPage />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/purchaseOrders`} element={<ProtectedRoute element={<PurchaseOrders />} isAuthenticated={isAuthenticated}/>} />
+        <Route  path={`${PREFIX_APP_PATH}/settings`} element={<ProtectedRoute element={<Settings />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac`} element={<ProtectedRoute element={<Alloptions />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/creategroup`} element={<ProtectedRoute element={<Creategroup />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/createpolicy`} element={<ProtectedRoute element={<Createpolicy />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/viewpolicy`} element={<ProtectedRoute element={<Viewpolicy />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/users`} element={<ProtectedRoute element={<UsersContent />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/roles`} element={<ProtectedRoute element={<RolesContent />} isAuthenticated={isAuthenticated}/>} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/createuser`} element={<ProtectedRoute element={<Createuser />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/groups`} element={<ProtectedRoute element={<GroupsContent />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/groups/viewgroupdetail`} element={<ProtectedRoute element={<Viewgroupdetail />}isAuthenticated={isAuthenticated} />} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/policies`} element={<ProtectedRoute element={<PoliciesContent />} isAuthenticated={isAuthenticated}/>} />
+        <Route  path={`${PREFIX_APP_PATH}/settings/rbac/permissions`} element={<ProtectedRoute element={<PermissionsContent />}isAuthenticated={isAuthenticated} />} />
 
         {/* Public Routes */}
-        <Route exact path={`${PREFIX_AUTH_PATH}/signin`} element={<Signin />} />
-        <Route exact path={`${PREFIX_AUTH_PATH}/signup`} element={<Signup />} />
-        <Route exact path={`${PREFIX_AUTH_PATH}/forgotPassword`} element={<ForgotPassword />} />
-        <Route exact path={`${PREFIX_AUTH_PATH}/otpVerification`} element={<OtpVerification />} />
-        <Route exact path={`${PREFIX_AUTH_PATH}/newPassword`} element={<NewPassword />} />
+        <Route  path={`${PREFIX_AUTH_PATH}/signin`} element={<Signin />} />
+        <Route  path={`${PREFIX_AUTH_PATH}/signup`} element={<Signup />} />
+        <Route  path={`${PREFIX_AUTH_PATH}/forgotPassword`} element={<ForgotPassword />} />
+        <Route  path={`${PREFIX_AUTH_PATH}/otpVerification`} element={<OtpVerification />} />
+        <Route  path={`${PREFIX_AUTH_PATH}/newPassword`} element={<NewPassword />} />
         <Route path="*" element={<PathNotFOund />} />
       </Routes>
     </Suspense>
